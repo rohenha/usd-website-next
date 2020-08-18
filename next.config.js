b8509bf5b2a79a2146bc2febbd707fd81d27bde8
rohenha <romain.breton.pro@gmail.com>
2020-08-18 09:59:31 +0200

    set dynamic teams

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 const withSass = require('@zeit/next-sass');
 
-module.exports = withSass({})
+module.exports = withSass({
+    env: {
+        NEXT_DATOCMS_API_TOKEN: '420d3cb14efc233d6082161fcdc659'
+    }
+})
 
