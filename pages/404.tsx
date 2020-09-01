ea06463256419c0dbab02badaeb27ba684b65a86
rohenha <romain.breton.pro@gmail.com>
2020-09-01 17:20:42 +0200

    fix 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,7 +2,7 @@ import { getDataMenu } from "Lib";
 import { GetStaticProps } from "next";
 
 export const getStaticProps: GetStaticProps = async () => {
-    const menu = await getDataMenu();
+    const menu = await getDataMenu('errorPage');
     return {
         props: {
             menu
